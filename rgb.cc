0d76bfd952e0492b79e80be695827891400fc727
Shumail92 <shumailmohyuddin@gmail.com>
2015-08-03 10:53:51 +0200

    LPM: PWM & RESET commands added.

--- rgb.cc
+++ rgb.cc
@@ -4,79 +4,78 @@
     #include "WProgram.h"
 #endif
 
-int led = 6;
-int brightness = 0;
-int fadeAmount = 5;
+#include <Adafruit_TLC5947.h>
 
-struct LED {
-    LED(uint8_t pin, int br) : pin(pin), brightness(br) {}
+Adafruit_TLC5947 io(1, 5, 4, 6);
 
-    void setup() {
-        pinMode(pin, OUTPUT);
-    }
-
-    void write() {
-        analogWrite(pin, brightness);
-    }
-
-    uint8_t pin;
-    int brightness;
-
-
-};
-
-struct Pulsor {
-    Pulsor(const LED &l, int step=5, int max_br = 255, int min_br = 0)
-            : led(l), min_br(min_br), max_br(max_br), step(step) { }
-
-    void pulse() {
-        int &brightness = led.brightness;
-
-        analogWrite(led.pin, led.brightness);
-
-        brightness += step;
-        if (brightness >= max_br) {
-            brightness = max_br;
-            step *= -1;
-        } else if (brightness <= min_br) {
-            brightness = min_br;
-            step *= -1;
-        }
-    }
-
-    int min_br;
-    int max_br;
-    int step;
-
-    LED led;
-};
-
-LED blue(6, 0);
-LED green(3, 125);
-LED red(5, 250);
-
-LED all(9, 255);
-
-Pulsor p_blue(blue, 1, 100);
-Pulsor p_green(green, 2);
-Pulsor p_red(red, 3);
-
-void setup() {                
-    blue.setup();
-    green.setup();
-    red.setup();
-
-    all.setup();
+void setup() {
+    io.begin();
 }
+
+//pwm 10,4096
+//reset
+//info
+//shoot
+
+struct command {
+    const char *command_id;
+    void (*func)(const String &args);
+};
+
+static void handle_pwm(const String &args) {
+    int pos = args.indexOf(',');
+    String pin_str = args.substring(0, pos);
+    String pwm_str = args.substring(pos + 1);
+
+    int pin = pin_str.toInt();
+    int pwm = pwm_str.toInt();
+
+    io.setPWM(pin, pwm);
+    io.write();
+
+    Serial.print("parsed pin val: ");
+    Serial.println(pin);
+
+    Serial.print("parsed pwm val: ");
+    Serial.println(pwm);
+}
+
+static void handle_reset(const String &args) {
+    io.reset();
+    io.write();
+}
+
+command known_cmds[] = {
+        {"pwm", handle_pwm},
+        {"reset", handle_reset},
+        {NULL, NULL}
+};
 
 void loop() {
 
-    p_red.pulse();
-    p_blue.pulse();
-    p_green.pulse();
+    if(Serial.available() > 0)  {
+        String input = Serial.readStringUntil('\r');
+        int pos = input.indexOf(' ');
 
-    all.write();
+        String cmd;
+        String args;
 
-    // wait for 30 milliseconds to see the dimming effect
-    delay(30);
+        if (pos != -1) {
+            cmd = input.substring(0, pos);
+            args = input.substring(pos+1);
+            Serial.println(cmd);
+        } else {
+            cmd = input;
+        }
+
+        for (command *iter = known_cmds; iter->command_id != NULL; iter++) {
+            if (cmd.equals(iter->command_id)) {
+                (*iter->func)(args);
+                break;
+            }
+        }
+
+        Serial.println("*");
+    }
+
 }
